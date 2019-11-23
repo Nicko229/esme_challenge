3578035320172331004cbbc3bf04258f901e7a82
Nick <3223136Crow@gmail.com>
2019-11-23 02:31:03 +0000

    css values changed to em except for image and personal details

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ const App = (props) => {
     props.fetchPosts();
   }, []);
 
+  console.log("posts", props.posts)
+
   const paginate = (pageNumber) => props.getCurrentPage(pageNumber)
 
   return (
